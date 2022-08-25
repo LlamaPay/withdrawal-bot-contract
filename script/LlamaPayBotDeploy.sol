658c4f753ac992f67c8bd7f8230bd50e7cfca3e3
nemusona <nemusonaneko@protonmail.com>
2022-08-25 10:48:21 -0500

    update deploy script

--- script/LlamaPayBotDeploy.sol
+++ script/LlamaPayBotDeploy.sol
@@ -9,7 +9,7 @@ contract LlamaPayBotDeploy is Script {
     function run() public {
         vm.startBroadcast();
         LlamaPayBot llamaPayBot = new LlamaPayBot{salt: bytes32("llama")}(
-            0xcCDd688d7eDcF89bFa217492E247d1395FcEC23D,
+            0xde1C04855c2828431ba637675B6929A684f84C7F,
             0xA43bC77e5362a81b3AB7acCD8B7812a981bdA478,
             0xad730D8e730c99E205A371436cE2e5aCFC38D7F9
         );
